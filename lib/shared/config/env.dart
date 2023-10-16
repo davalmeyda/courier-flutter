febaa462ea0622ec56c2f1b5a2923c1913ad6dd4
David Almeyda <dav.almeyda@gmail.com>
2023-10-16 11:01:46 -0500

    cambios

--- lib/shared/config/env.dart
+++ lib/shared/config/env.dart
@@ -1,4 +1,4 @@
 class EnvironmentVariables {
-  // static const String baseUrl = "http://192.168.18.115:3000/";
-  static const String baseUrl = "http://161.132.42.80:32081/";
+  // static const String baseUrl = "http://192.168.18.253:3000/";
+  static const String baseUrl = "https://courier.service.ojo-plomo.com/";
 }
